5acdc38fe144641df05175128a6157b952aed808
Ruslan Yuzefovych <lossantos2016@gmail.com>
2018-06-26 14:19:03 +0300

    nothing

---
+++ src/ArithmeticAverage.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Ruslan on 01.08.2016.
+ */
+
+public class ArithmeticAverage {
+    public static void main (String[]args) {
+        int x = 2;
+        byte y = 7;
+        double z = 14.1;
+        System.out.println("Arithmetic Average " + (x+y+z)/3);
+    }
+
+}
